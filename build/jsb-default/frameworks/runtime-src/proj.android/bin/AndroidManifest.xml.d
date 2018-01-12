c0a423302b57cbc075b18bb667682a7d2b6958ad
yinjimmy <jimmy.yin5@gmail.com>
2018-01-12 14:57:39 +0800

    cleanup

--- build/jsb-default/frameworks/runtime-src/proj.android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/admin/fb17/build/jsb-default/frameworks/runtime-src/proj.android/bin/AndroidManifest.xml : \
-/Users/admin/fb17/build/jsb-default/frameworks/runtime-src/proj.android/AndroidManifest.xml \
--- build/jsb-default/frameworks/runtime-src/proj.android/plugin_review_res_project/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/admin/fb17/build/jsb-default/frameworks/runtime-src/proj.android/plugin_review_res_project/bin/AndroidManifest.xml : \
-/Users/admin/fb17/build/jsb-default/frameworks/runtime-src/proj.android/plugin_review_res_project/AndroidManifest.xml \
